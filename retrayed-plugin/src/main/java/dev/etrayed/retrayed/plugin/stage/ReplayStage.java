8b152d9207e2c81f708215ab1f59aa76a22b770a
Etrayed <etrayedyt@gmail.com>
2020-10-01 15:44:09 +0200

    feat: entityId allocator

--- retrayed-plugin/src/main/java/dev/etrayed/retrayed/plugin/stage/ReplayStage.java
+++ retrayed-plugin/src/main/java/dev/etrayed/retrayed/plugin/stage/ReplayStage.java
@@ -7,17 +7,33 @@ import com.google.common.collect.Maps;
 import dev.etrayed.retrayed.api.play.PlaybackPacketListener;
 import dev.etrayed.retrayed.plugin.play.PlaybackImpl;
 import dev.etrayed.retrayed.plugin.stage.entity.ReplayEntity;
+import org.bukkit.Bukkit;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Etrayed
  */
 public class ReplayStage {
+
+    private static final Field ENTITY_COUNT_FIELD;
+
+    static {
+        String serverPackage = Bukkit.getServer().getClass().getPackage().getName();
+
+        try {
+            ENTITY_COUNT_FIELD = Class.forName("net.minecraft.server." + serverPackage.substring(serverPackage
+                    .lastIndexOf('.') + 1) + ".Entity").getDeclaredField("entityCount");
+        } catch (ClassNotFoundException | NoSuchFieldException e) {
+            throw new InternalError(e);
+        }
+    }
 
     private final PlaybackImpl playback;
 
@@ -59,7 +75,19 @@ public class ReplayStage {
     }
 
     private int nextEntityId() {
-        return -1;
+        try {
+            if(AtomicInteger.class.isAssignableFrom(ENTITY_COUNT_FIELD.getType())) {
+                return ((AtomicInteger) ENTITY_COUNT_FIELD.get(null)).incrementAndGet();
+            }
+
+            int nextId = ENTITY_COUNT_FIELD.getInt(null) + 1;
+
+            ENTITY_COUNT_FIELD.setInt(null, nextId);
+
+            return nextId;
+        } catch (IllegalAccessException e) {
+            throw new InternalError(e); // Cannot happen since Entity#entityCount it static
+        }
     }
 
     public List<PlaybackPacketListener> listeners() {
